793c35fd34ac683f736c3df3037cdba3a2d13090
kazuya-minatani <waaawooo37ta2@gmail.com>
2021-05-22 21:57:23 +0900

    バリデーションView側

--- src/main/java/com/example/demo/ShainController.java
+++ src/main/java/com/example/demo/ShainController.java
@@ -2,12 +2,20 @@ package com.example.demo;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 //コントローラーの宣言
 @Controller
 public class ShainController {
+	
+//	バリデーション用に作成したbeanを初期化して使えるようにする *requestmappingの前に記述する必要がある
+	@ModelAttribute
+	public ShainFrom setUpForm() {
+		return new ShainFrom();
+	}
 	
 //	URLパターンの設定
 	@RequestMapping("/input")
@@ -15,11 +23,19 @@ public class ShainController {
 		return "index.html";
 	}
 	
+	
 //	thアクションの指定で一致する箇所はMappingで指定した記述
 	@RequestMapping("/output")
 	
+//	バリデーション用　入力チェック後の挙動記述
+	public String result(@Validated ShainFrom shainFrom,
+			BindingResult bindeingResult, Model model) {
+		if(bindeingResult.hasErrors()) {
+			return "index.html";
+		}
+	
+	String number = shainFrom.getNumber();
 //	@RequestParamで送信するデータのデータ型を指定している
-	public String result(@RequestParam("number") String number, Model model) {
 		String name = "コントローラー";
 		model.addAttribute("number", number);
 		model.addAttribute("name", name);
--- src/main/java/com/example/demo/ShainFrom.java
+++ src/main/java/com/example/demo/ShainFrom.java
@@ -3,8 +3,13 @@
 import javax.validation.constraints.NotEmpty;
 
 public class ShainFrom{
+//		空の場合messageを返す
 	 	@NotEmpty(message = "社員Noを入れてください")
+	 	
+//	 	index.htmlでのinputタグで設定したname
 	 	private String number;
+	 	
+//	 	number変数を作成したらGetter、Setterを作成することがお約束
 	 	public String getNumber() {
 	 		return number;
 	 	}
